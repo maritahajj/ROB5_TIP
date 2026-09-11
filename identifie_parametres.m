937a6d0938f9cf0a51f0fdee390b351739e18538
maritahajj <71569412+maritahajj@users.noreply.github.com>
2026-09-11 17:58:20 +0200

    Add files via upload

--- identifie_parametres.m
+++ identifie_parametres.m
@@ -1,24 +1,35 @@
+
 Phi = @(x) [-x(1) x(2)-x(1) 0 -x(4) x(5)-x(4) 0 -x(7) 0 0;
            0 x(1)-x(2) x(3)-x(2) 0 x(4)-x(5) x(6)-x(5) 0 -x(8) 0;
            0 0 x(2)-x(3) 0 0 x(5)-x(6) 0 0 -x(9)];
 
+
+
+
 list_idx = [5 10 50 100 150 200 300 500 700];
-A = [];
+A= [];
 Y = [];
-
-for k=1:numel(list_idx)
+for k = 1:numel(list_idx)
     idx = list_idx(k);
-     if(quanti == false)
+    if(quanti == false)
         x = [alpha(idx) beta(idx) gamma(idx) vit_alpha(idx) vit_beta(idx) vit_gamma(idx) acc_alpha(idx) acc_beta(idx) acc_gamma(idx)];
-     else
+    else
          x = [pos_q*round(alpha(idx)*1/pos_q) pos_q*round(beta(idx)*1/pos_q) pos_q*round(gamma(idx)*1/pos_q) vit_q*round(vit_alpha(idx)*1/vit_q) vit_q*round(vit_beta(idx)*1/vit_q) vit_q*round(vit_gamma(idx)*1/vit_q) acc_q*round(acc_alpha(idx)*1/acc_q) acc_q*round(acc_beta(idx)*1/acc_q) acc_q*round(acc_gamma(idx)*1/acc_q)];
-     end
-     if(filter == true)
-         x = filtfilt([1 u-1], u, x);
-     end
-
-     A = [A ; Phi(x)];
-     Y = [Y; -1;0;0];
+    end
+    x_filt = filtfilt([1 u-1], u, x);
+    figure(1);
+    plot([1:1:9],x_filt);
+    hold on;
+    plot([1:1:9],x);
+    hold off;
+    if(filter == true)
+        x = x_filt;
+    end
+    A = [A ; Phi(x)];
+    Y = [Y; -1;0;0];
 end
 
+
 x = A\Y;
+
+
--- question4.m
+++ question4.m
@@ -7,12 +7,10 @@ vit_q = 0.1 * 1e-3;
 acc_q = 1 * 1e-3;
 
 filter = true;
-u = 0.9;
+u = 0.999;
 
 run("definit_param.m");
-k1 = 5000;
-b1 = 400;
-x_reel = [k0;k1;k2;b0;b1;b2;m1;m2;m3];
+
 run("simule_systeme.m");
 run ("identifie_parametres.m");
 
