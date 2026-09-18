66a7a0231e45587324971bdc679c8c71f0d4c313
maritahajj <71569412+maritahajj@users.noreply.github.com>
2026-09-18 08:54:03 +0200

    Add files via upload

--- question3.m
+++ question3.m
@@ -1,6 +1,8 @@
 clear all;
 close all;
 
+u = 0.99;
+filter = true;
 quanti = true;
 pos_q = 10 * 1e-6;
 vit_q = 0.1 * 1e-3;
@@ -11,7 +13,7 @@ k1 = 5000;
 b1 = 400;
 x_reel = [k0;k1;k2;b0;b1;b2;m1;m2;m3];
 run("simule_systeme.m");
-run ("identifie_parametres.m");
+run("identifie_parametres.m");
 
 disp("parametre trouve: ")
 disp(x);
