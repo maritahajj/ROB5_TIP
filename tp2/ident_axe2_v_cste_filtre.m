4c36ca96bc6e0aa0d57bf8e43cd47f6179b55352
maritahajj <71569412+maritahajj@users.noreply.github.com>
2026-09-18 10:16:07 +0200

    manipulation_question1

--- tp2/ident_axe2_v_cste_filtre.m
+++ tp2/ident_axe2_v_cste_filtre.m
@@ -6,11 +6,11 @@
 close all
 clc
 clear all; %% efface toutes les variables existantes
-load releve_vit_cste_axe2; %% charge les relevés expérimentaux
+load releve_vit_cste_axe2; %% charge les relevÃ©s expÃ©rimentaux
 
-%% Paramètres connus a priori:
+%% ParamÃ¨tres connus a priori:
 kc2=0.0525; %% constante de couple de l'axe 2.
-N2=4.5; %% inverse du rapport de réduction de l'axe 2.
+N2=4.5; %% inverse du rapport de rÃ©duction de l'axe 2.
 
 kc1=0.0525;
 N1=20.25;
@@ -22,9 +22,9 @@ s = size(q2);
 A = [];
 y = [];
 for k=1:s(1)
-    a = [cos(q2(k)) sign(qp2(k)) qp2(k) 1];
+    a = [cos(q2(k)) sign(qpfil2(k)) qpfil2(k) 1];
     A = [A; a];
-    y = [y; N2*kc2*i2(k)];
+    y = [y; N2*kc2*ifil2(k)];
 end
 
 disp("Size de A : ");
@@ -35,23 +35,23 @@ disp("Rank de A : ");
 disp(rank(A));
 disp("Conditionnement de A : ");
 disp(cond(A));
-%% Calcul des paramètres
+%% Calcul des paramÃ¨tres
 p = (A' * A)\(A' * y);
 
-%% Affichage des résultats.
+%% Affichage des rÃ©sultats.
 format long
-disp('Paramètres estimés à partir des données brutes :');
+disp('ParamÃ¨tres estimÃ©s Ã  partir des donnÃ©es brutes :');
 p'
 
 figure(1)
 clf; %% clear figure
-h=plot3(q2,qp2,kc2*N2*i2,'x');
+h=plot3(q2,qpfil2,kc2*N2*i2,'x');
 set(h,'LineWidth',0.5);
-hold on; %% permet de conserver le graphique et d'en ajouter d'autres sur la même fig.
-h=plot3(q2,qp2,A*p,'.');
+hold on; %% permet de conserver le graphique et d'en ajouter d'autres sur la mÃªme fig.
+h=plot3(q2,qpfil2,A*p,'.');
 set(h,'LineWidth',1.5);
-title('Résultats de l''identification sans filtrage');
-legend('\Gamma_2 non filtré', 'modèle');
+title('RÃ©sultats de l''identification sans filtrage');
+legend('\Gamma_2 non filtrÃ©', 'modÃ¨le');
 grid on;
 xlabel('$q_2$','Interpreter','latex')
 ylabel('$\dot{q}_2$','Interpreter','latex')
